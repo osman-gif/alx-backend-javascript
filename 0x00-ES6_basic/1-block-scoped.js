4767335b006000aee81abbd8a15b0e35d57f8b28
osman-gif <osmanngong@gmail.com>
2024-10-01 11:20:53 +0200

    modified the taskBlock function to user only var variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-
-  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-  }
   var task = false;
   var task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
   return [task, task2];
 }
