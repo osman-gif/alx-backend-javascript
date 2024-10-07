cbc3db91e8d4a1be9ab830c4aa5ea32488ed5d18
osman-gif <osmanngong@gmail.com>
2024-10-07 10:21:23 +0200

    made the 4-rest-parameter.js file pass eslint

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 export default function returnHowManyArguments(...args) {
   let count = 0;
-  for (let i = 0; i < args.length; i++){
-    count++;
+  for (let i = 0; i < args.length; i += 1) {
+    count += 1;
   }
   return count;
 }
