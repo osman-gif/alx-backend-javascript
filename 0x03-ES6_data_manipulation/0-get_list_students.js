e5a72d18a93261e5a89060d2c7bbfc3a4778d327
osman-gif <osmanngong@gmail.com>
2024-10-08 11:01:51 +0200

    added the 0x03-manipulation directory

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents(){
+  return [{
+    'id': 1,
+    'fristName': 'Guillaume',
+    'location': 'San Francisco'
+  },
+  {
+    'id': 2,
+    'fristName': 'James',
+    'location': 'Columbia'
+  },
+  {
+    'id': 5,
+    'fristName': 'Serena',
+    'location': 'San Francisco'
+  },
+  ]
+}
