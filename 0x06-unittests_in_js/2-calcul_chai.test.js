d57267c03999f95ed0cb0177f7d9918a6f03b2d0
osman-gif <osmanngong@gmail.com>
2024-11-28 11:56:33 +0200

    Added 2-calcul_chai.js 2-calcul_chai.test.js files

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,33 @@
+// 1. In the test file, write tests for each function in the 1-calcul.js file.
+const calculateNumber = require("./1-calcul.js");
+import { expect } from "cahi";
+
+describe("calculateNumber", function(){
+    describe("type SUM", function(){
+        it("should return 4", function(){
+            expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+        });
+        it("should return 4", function(){
+            expect(calculateNumber('SUM', 1.1, 3)).to.equal(4);
+        });
+    });
+    describe("type SUBTRACT", function(){
+        it("should return -2", function(){
+            expect(calculateNumber('SUBTRACT', 1, 3), ).to.equal(-2);
+        });
+        it("should return -2", function(){
+            expect(calculateNumber('SUBTRACT', 1.1, 3)).to.equal(-2);
+        });
+    });
+    describe("type DIVIDE", function(){
+        it("should return 0.3333333333333333", function(){
+            expect(calculateNumber('DIVIDE', 1, 3)).to.equal(0.3333333333333333);
+        });
+        it("should return 0.3333333333333333", function(){
+            expect(calculateNumber('DIVIDE', 1.1, 3)).to.equal(0.3333333333333333);
+        });
+        it("should retrun error", function(){
+            expect(calculateNumber('DIVIDE', 1, 0)).to.equal("Error");
+        });
+    });
+});
