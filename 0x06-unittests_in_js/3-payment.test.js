90e7f3d3389adcbbc3e2a62e7e0193337e622e4c
osman-gif <osmanngong@gmail.com>
2024-11-30 13:22:29 +0200

    made some changes to the file 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,13 @@
 import sinon from 'sinon';
 import {sendPaymentRequestToApi} from './payment';
 
+
 describe('sendPaymentRequestToApi', function() {
-    let consoleSpy;
-    beforeEach(() => {
-        consoleSpy = sinon.spy(console, 'log');
-    });
-
-    afterEach(() => {
-        consoleSpy.restore();
-    });
-
-    it('should log the right output', function() {
+    it('should return 120', function() {
+        const spy = sinon.spy(console, 'log');
         sendPaymentRequestToApi(100, 20);
-        expect(consoleSpy.calledOnceWithExactly('The total is: 120')).toBe(true);
+        spy.restore();
+        sinon.assert.calledWith(spy, 'The total is: 120');
     });
 }
 );
